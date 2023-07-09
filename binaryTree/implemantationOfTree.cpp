08b8e1a5e153f59305164f6dabc7e3ea0a1ece27
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2023-07-10 01:34:53 +0530

    implemantation of binary tree

---
+++ binaryTree/implemantationOfTree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data) {
+         this->data = data;
+         this->left = NULL;
+         this->right = NULL; 
+    }
+};
+
+Node *createTree()
+{
+    cout<<"Enter the data : ";
+    int data;
+    cin>>data;
+    if(data == -1){
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+
+    cout<<"Enter for the left of "<<data<<endl;
+    root->left = createTree();
+
+    cout<<"Enter for the right of "<<data<<endl;
+    root->right = createTree();
+
+    return root;
+
+}
+
+
+
+int main()
+{
+    
+    Node *root = createTree();
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+}
