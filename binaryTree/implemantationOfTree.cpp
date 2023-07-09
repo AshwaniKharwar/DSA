ece14609708c30310dd16da969ae8ef3bdcf417c
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2023-07-10 01:48:46 +0530

    implemantation of level order traversal

--- binaryTree/implemantationOfTree.cpp
+++ binaryTree/implemantationOfTree.cpp
@@ -38,11 +38,3 @@ Node *createTree()
 
 }
 
-
-
-int main()
-{
-    
-    Node *root = createTree();
-    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-}
---
+++ binaryTree/levelOrderTraversal.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include "implemantationOfTree.cpp"
+
+using namespace std;
+
+void levelOrderTravesal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main(){
+    Node* root = createTree();
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+
+    cout<<" Level order Traversal "<<endl;
+    levelOrderTravesal(root);
+}
