8525b41a03505fa17ed84629c97fa8958433aff7
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2023-07-13 10:37:23 +0530

    Build tree from level order traversal

---
+++ binaryTree/buildTreeFromLevelOrderTraversal.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<queue>
+#include "implemantationOfTree.cpp"
+using namespace std;
+
+void buildTreeFromLevalOrderTraveral(Node* &root){
+    queue<Node*>q;
+    cout<<"Enter data for root node: ";
+    int data;
+    cin>>data;
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter data for left of "<<temp->data<<": ";
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata != -1){
+            temp->left = new Node(leftdata);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter data for right of "<<temp->data<<": ";
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata != -1){
+            temp->right = new Node(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
+
+void levelOrderTravesal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main(){
+    Node *root = NULL;
+    buildTreeFromLevalOrderTraveral(root);
+
+    levelOrderTravesal(root);
+}
